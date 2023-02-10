d81886227e5e0e5620b181070fe85b5146ed534e
Dmitry Lyapin <lyapindm@yandex.ru>
2023-02-10 15:58:55 +0300

    Сделал поиск пользователей по парметрам для менеджера

--- src/modules/manager/manager.controller.ts
+++ src/modules/manager/manager.controller.ts
@@ -1,4 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { Get, Query, UseGuards } from '@nestjs/common';
+import { Roles } from 'src/decorators/roles.decorator';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { AdminService } from '../admin/admin.service';
+import { SearchUserParams } from '../users/types/search-user-params';
 
+@UseGuards(RolesGuard)
 @Controller('manager')
-export class ManagerController {}
+export class ManagerController {
+    constructor(private readonly adminService: AdminService) {}
+
+    @Get('users')
+    @Roles('manager')
+    async getUsers(@Query() query: SearchUserParams) {
+        return await this.adminService.getUsers(query);
+    }
+}
--- src/modules/manager/manager.module.ts
+++ src/modules/manager/manager.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from '../admin/admin.module';
 import { ManagerController } from './manager.controller';
 
 @Module({
+    imports: [AdminModule],
     controllers: [ManagerController],
 })
 export class ManagerModule {}
