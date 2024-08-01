18d356a0858a37baa63f6eda33fb25c1aa880007
Dmitry Lyapin <lyapindm@yandex.ru>
2024-08-01 16:13:09 +0300

    Поправил темплейт для почты

--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -12,7 +12,7 @@ export class MailService {
     ) {
         await this.mailerService.sendMail({
             to: user.email,
-            // from: '"Support Team" <support@example.com>', // override default from
+            from: '"Top-Hotels.su" <Top-Hotels.su@yandex.ru>', // override default from
             subject: 'Добро пожаловать на Top-Hotels.su! Подтвердите Ваш Email',
             template: './confirm-email',
             // either change to ./transactional or rename transactional.html to confirmation.html
