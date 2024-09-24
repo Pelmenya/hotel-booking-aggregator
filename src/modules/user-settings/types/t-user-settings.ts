eb0b9614f96dc28ae888d27006cd845101fd01f5
Dmitry Lyapin <lyapindm@yandex.ru>
2024-09-24 11:29:35 +0300

    Добавил поле theme в типы

--- src/modules/user-settings/types/t-user-settings.ts
+++ src/modules/user-settings/types/t-user-settings.ts
@@ -1,9 +1,11 @@
 import { TCurrency } from './t-currency';
 import { TLanguage } from './t-language';
+import { TTheme } from './t-theme';
 
 export type TUserSettings = {
     id: number;
     userId: string;
+    theme: TTheme;
     language: TLanguage;
     currency: TCurrency;
     phoneChanel: boolean;
--- src/modules/user-settings/types/update-user-settings.dto.ts
+++ src/modules/user-settings/types/update-user-settings.dto.ts
@@ -1,9 +1,11 @@
 import { IsBoolean, IsOptional, IsString } from 'class-validator';
 import { TLanguage } from './t-language';
 import { TCurrency } from './t-currency';
+import { TTheme } from './t-theme';
 
 export class UpdateUserSettingsDTO {
     @IsOptional() @IsString() language?: TLanguage;
+    @IsOptional() @IsString() theme?: TTheme;
     @IsOptional() @IsString() currency?: TCurrency;
     @IsOptional() @IsString() phoneChanel?: boolean;
     @IsOptional() @IsBoolean() emailChanel?: boolean;
