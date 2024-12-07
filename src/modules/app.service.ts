f87ba65a5f4a8ec4470d8f3dadcbea7a93198acf
Dmitry Lyapin <lyapindm@yandex.ru>
2024-12-07 16:21:34 +0300

    Start github action

--- src/modules/app.service.ts
+++ src/modules/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'API Hotel Booking Aggregator';
+        return 'API Hotel Booking Aggregator V1.0';
     }
 }
