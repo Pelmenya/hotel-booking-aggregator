3a1b0595ecb7a35307b61c347d378cfc361b0f4f
Dmitry Lyapin <lyapindm@yandex.ru>
2024-12-18 10:48:01 +0300

    Add methods to hotelsRepository

--- src/modules/hotels/hotels.repository.ts
+++ src/modules/hotels/hotels.repository.ts
@@ -10,10 +10,19 @@ export class HotelsRepository {
         @InjectRepository(Hotels)
         private readonly hotelsRepository: Repository<Hotels>,
     ) {}
+
+    async findOneById(id: string): Promise<Hotels> {
+        return await this.hotelsRepository.findOneBy({ id });
+    }
+
+    async updateHotelIsVisible(id: string, is_visible: boolean): Promise<void> {
+        await this.hotelsRepository.update(id, { is_visible });
+    }
+
     async searchHotels(query: SearchBaseParams): Promise<Hotels[]> {
         const q = this.hotelsRepository
             .createQueryBuilder('hotel')
-            .innerJoinAndSelect('hotel.locations', 'location')
+            .leftJoinAndSelect('hotel.locations', 'location')
             .andWhere(
                 'hotel.name ILIKE :q OR hotel.name_en ILIKE :q OR hotel.address ILIKE :q OR location.address ILIKE :q',
                 {
@@ -22,7 +31,7 @@ export class HotelsRepository {
             )
             .orderBy('hotel.rating', 'DESC');
 
-        q.limit(query.limit);
+        q.limit(query.limit * 2); // Английский и русский язык геолакаций, поэтому на 2
         q.offset(query.offset);
 
         return q.getMany();
