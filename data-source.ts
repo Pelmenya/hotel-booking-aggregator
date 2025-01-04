d2f4755f6cd74567a2b4193abd74444c2477c4ba
Dmitry Lyapin <lyapindm@yandex.ru>
2025-01-04 16:45:53 +0300

    Fix DataSource host

--- data-source.ts
+++ data-source.ts
@@ -6,7 +6,7 @@ config();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'host.docker.internal',
+    host: process.env.POSTGRES_HOST,
     port: parseInt(process.env.POSTGRES_PORT, 10),
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
